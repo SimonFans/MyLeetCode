87442672819c1d3add095bb045677c6975d1f080
Ximeng Zhao <simon.zhao@simonzhao-ltm.internal.salesforce.com>
2022-04-07 20:49:37 -0700

    a

---
+++ Leetcode/Backtracking/Combination Sum Number Used Once.py
@@ -0,0 +1,42 @@
+'''
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output:
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+
+Input: candidates = [2,5,2,1,2], target = 5
+Output:
+[
+[1,2,2],
+[5]
+]
+'''
+
+class Solution:
+
+    # 和上一道题类似。只不过这道题要求candidate中的每个数只能使用一次。也是使用dfs。
+    def combinationSum2(self, candidates: 'List[int]', target: 'int') -> 'List[List[int]]':
+        def helper(nums, start, path, ans, remain):
+            if remain == 0:
+                ans.append(path)
+                return
+
+            for i in range(start, len(candidates)):
+                # if [1 1 1] , target = 2 then second part 1 1 will not run
+                #       s i
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+                # if current value is greater than the remain value then no need to continue
+                if candidates[i] > remain:
+                    break
+                helper(candidates, i + 1, path + [candidates[i]], ans, remain - candidates[i])
+
+        # aviod over counting         
+        candidates.sort()
+        ans = []
+        helper(candidates, 0, [], ans, target)
+        return ans
