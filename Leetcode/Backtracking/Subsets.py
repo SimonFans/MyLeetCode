73badd88e655f3759b4a6b74fc7f26aa13fc8a9d
Ximeng Zhao <simon.zhao@simonzhao-ltm.internal.salesforce.com>
2022-04-08 11:03:15 -0700

    a

--- Leetcode/Backtracking/Subsets.py
+++ Leetcode/Backtracking/Subsets.py
@@ -19,3 +19,31 @@ class Solution:
         nums.sort()
         helper(0, [])
         return ans
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        '''
+        Backtracking is an algorithm for finding all solutions by exploring all potential candidates. If the solution candidate turns to be not a solution (or at least not the last one), backtracking algorithm discards it by making some changes on the previous step, i.e. backtracks and then try again.
+        '''
+        def backtrack(first=0, curr=[]):
+            # if the current combination is done  which means length of curr == k
+            if len(curr) == k:
+                ans.append(curr[:])
+                return
+            for i in range(first, n):
+                curr.append(nums[i])
+                backtrack(i+1, curr)
+                print(curr)
+                curr.pop()
+                print(curr)
+
+        ans = []
+        n = len(nums)
+        # why n+1 because you want to cover empty case, k => all subsets of length k
+        for k in range(n+1):
+            backtrack()
+        return ans
+
+Time: O(2^N * N)  to generate all subsets and then copy them into output list.
+Space: O(N)
