573ea98819eb695fe32c3d50adc9326728f3e6ff
Ximeng Zhao <simon.zhao@simonzhao-ltm.internal.salesforce.com>
2022-04-13 09:33:17 -0700

    a

---
+++ Leetcode/BFS/Word Ladder Print All Path.py
@@ -0,0 +1,28 @@
+import collections
+from collections import *
+import string
+class Solution(object):
+    def findLadders(self, beginWord, endWord, wordList):
+        wordList = set(wordList)
+        res = []
+        layer = {}
+        layer[beginWord] = [[beginWord]]
+        while layer:
+            newlayer = collections.defaultdict(list)
+            for w in layer:
+                if w == endWord:
+                    res.extend(k for k in layer[w])
+                else:
+                    for i in range(len(w)):
+                        for c in string.ascii_lowercase:
+                            neww = w[:i]+c+w[i+1:]
+                            if neww in wordList:
+                                newlayer[neww]+=[j+[neww] for j in layer[w]]
+            wordList -= set(newlayer.keys())
+            layer = newlayer
+        return res
+
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log","cog"]
+print(Solution().findLadders(beginWord,endWord, wordList))
