3963a29a88ea7e841111e42d7b474f68d37c54aa
Ximeng Zhao <simon.zhao@simonzhao-ltm.internal.salesforce.com>
2022-04-14 10:33:42 -0700

    a

--- Leetcode/Array/Product of Array Except Itself.py
+++ Leetcode/Array/Product of Array Except Itself.py
@@ -8,11 +8,9 @@ Output: [0,0,9,0,0]
 example 1 idea:
 1           2       3        4
 1           1      1*2      1*2*3  ->
-1*2*3*4   1*3*4    1*2*4     1     <-
+1*2*3*4   1*3*4    1*2*4     1     <- your answer
 
 '''
-
-
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
