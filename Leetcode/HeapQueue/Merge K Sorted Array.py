888ad66444cb27927b231c3db338105926463075
Ximeng Zhao <simon.zhao@simonzhao-ltm.internal.salesforce.com>
2022-04-24 12:18:20 -0700

    a

--- Leetcode/HeapQueue/Merge K Sorted Array.py
+++ Leetcode/HeapQueue/Merge K Sorted Array.py
@@ -17,3 +17,26 @@ while minHeap:
     if next_arr:
         heapq.heappush(minHeap, (next_arr[0], next_arr[1:]))
 print(res)
+
+
+#### 合并两个sorted array in place
+arr1 = [1,2,3]
+arr2 = [2,5,6,8]
+
+# guarantee arr1 always the longest
+if len(arr1) < len(arr2):
+    arr1, arr2 = arr2, arr1
+# m always the long array length
+# n always the short array length
+m, n = len(arr1), len(arr2)
+arr1.extend([0]*n)
+while m > 0 and n > 0:
+    if arr1[m-1] > arr2[n-1]:
+        arr1[m+n-1] = arr1[m-1]
+        m -= 1
+    else:
+        arr1[m+n-1] = arr2[n-1]
+        n -= 1
+if n > 0:
+    arr1[:n] = arr2[:n]
+print(arr1)
